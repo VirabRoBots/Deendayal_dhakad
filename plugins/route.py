b970172c3aac50750b06b22bad700a05b500c4b2
Virab_RoBots <114412453+VirabRoBots@users.noreply.github.com>
2026-08-21 21:19:12 +0530

    Fix indentation and add newline at end of file

--- plugins/route.py
+++ plugins/route.py
@@ -231,4 +231,4 @@ async def media_streamer(request: web.Request, id: int, secure_hash: str):
             "Content-Disposition": f'{disposition}; filename="{file_name}"',
             "Accept-Ranges": "bytes",
         },
-    )
+                )
